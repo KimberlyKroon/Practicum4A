2582aa696f66aa28e77b39f3e78ff5d446119722
Kimberly Kroon <321231@student.mboutrecht.nl>
2022-02-25 12:09:25 +0100

    Initial commit

---
+++ src/Huis.java
@@ -0,0 +1,20 @@
+public class Huis {
+    private String adres;
+    private int bouwjaar;
+
+
+    public Huis(String adr, int bwjr){
+        adres = adr;
+        bouwjaar = bwjr;
+
+    }
+    public void setHuisbaas(Persoon hb){
+        hb = Persoon;
+    }
+    public Persoon getHuisbaas(){
+        return hb;
+    }
+
+
+
+}
---
+++ src/Persoon.java
@@ -0,0 +1,10 @@
+public class Persoon {
+    private String naam;
+    private int leeftijd;
+
+    public Persoon(String nm, int lft){
+        naam = nm;
+        leeftijd = lft;
+    }
+
+}
